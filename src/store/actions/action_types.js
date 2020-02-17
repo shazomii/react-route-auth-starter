f702562b710728e9dbb9174af32511ecd1e47a17
shazomii <seyeoyeniran2u@gmail.com>
2020-02-17 23:24:53 +0100

    indicate boilerplate actions

--- src/store/actions/action_types.js
+++ src/store/actions/action_types.js
@@ -1,5 +1,5 @@
-export const SUCCESS = "SUCCESS";
-export const FAILURE = "FAILURE";
+export const SUCCESS = "SUCCESS"; //boilerplate action
+export const FAILURE = "FAILURE"; //boilerplate action
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 export const ADD_PROFILE = "ADD_PROFILE";
